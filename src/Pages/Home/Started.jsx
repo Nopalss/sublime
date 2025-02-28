2c68b164b5f13a5c7a6d61cc6a54aea8c207114e
nopalss <holanakdo@gmail.com>
2025-02-28 17:54:29 +0700

    fixed started page

--- src/Pages/Home/Started.jsx
+++ src/Pages/Home/Started.jsx
@@ -15,7 +15,7 @@ const Started = () => {
 
 const TextStarted = () => {
   return (
-    <div className="flex flex-col gap-6 w-2/5">
+    <div className="flex flex-col gap-6 w-2/5 mt-[4em]">
       <p className="text-lg text-gray-400">55,000+ trusted Businesses</p>
       <h1 className="text-7xl">Build your next project faster.</h1>
       <p className="text-base text-gray-500">
@@ -25,7 +25,7 @@ const TextStarted = () => {
       <div className="flex w-full justify-between py-2 px-1 border border-gray-200  rounded-full">
         <input
           type="text"
-          className="outline-none px-2"
+          className="outline-none px-2 w-3/5"
           placeholder="Enter your mail address"
         />
         <Button>Get Started</Button>
@@ -46,8 +46,8 @@ const TextStarted = () => {
 
 const ImageStarted = () => {
   return (
-    <div className="w-2/5 relative">
-      <div className=" bg-gradient-to-r w-[845px] h-[344px] from-[#FFDD00] to-[#46F883] opacity-56 blur-[99px]"></div>
+    <div className="w-3/5 relative">
+      <div className="absolute top-[10em] bg-gradient-to-r w-full h-[344px] from-[#FFDD00] to-[#46F883] opacity-56 blur-[99px]"></div>
       <img src={page} alt="" className="absolute -right-20 z-10 top-0" />
     </div>
   );
