242299831ae47962c30a2e0eff6b95a44f893f76
Wisnu Aji Pamungkas <wisnuajipamungkas354@gmail.com>
2025-03-03 11:19:01 +0700

    fix testimonials

--- src/Pages/Home/Testimonial.jsx
+++ src/Pages/Home/Testimonial.jsx
@@ -6,6 +6,7 @@ import tribal from '../../img/icon/tribal.png';
 import veroxfloor from '../../img/icon/veroxfloor.png';
 import xpeng from '../../img/icon/xpeng.png';
 import profileIcon from '../../img/profileIcon.png';
+import profileJohn from '../../img/profile/profile3.png';
 import { Rating, ThinStar } from '@smastrom/react-rating';
 import '@smastrom/react-rating/style.css';
 
@@ -56,11 +57,11 @@ const Testimonial = () => {
               <Rating style={{ maxWidth: 150}} value={4} itemStyles={styleRating} readOnly/>
               <p className='italic'>"It’s intuitive, functional, easy-to-setup and presents content in an interactive and interesting way."</p>
               <div className="flex flex-row gap-3 items-center">
-                <img src={profileIcon} alt="Icon Profile" className="w-15" />
+                <img src={profileJohn} alt="Icon Profile" className="w-15" />
                 <span>
-                  <p className="font-semibold">Sarah Smith</p>
+                  <p className="font-semibold">John Smith</p>
                   <p className="text-sm text-gray-500">
-                    Marketing Leader @Mailchimp
+                    UI Designer @Flowbase
                   </p>
                 </span>
               </div>
@@ -68,13 +69,13 @@ const Testimonial = () => {
           </div>
           <div className="absolute z-10 w-full h-full bg-gradient-to-r from-[#46F883] to-[#FFDD00] opacity-36 blur-[99px] rounded-full border-b-2 border-black"></div>
         </div>
-        <div className='flex flex-col gap-6'>
+        <div className='flex flex-col gap-10 px-36'>
           <h4 className='uppercase text-gray-500 text-center'>Clients Using Sublime</h4>
           <span className='flex flex-row gap-3 justify-between items-center'>
-            <img src={invoice2go} className='w-40' />
-            <img src={tribal} className='w-30' />
-            <img src={xpeng} className='w-40' />
-            <img src={veroxfloor} className='w-40' />
+            <img src={invoice2go} className='w-30' />
+            <img src={tribal} className='w-20' />
+            <img src={xpeng} className='w-30' />
+            <img src={veroxfloor} className='w-30' />
           </span>
         </div>
       </div>
