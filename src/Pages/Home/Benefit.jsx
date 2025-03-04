5693bfe0104636e950aba6f3df2f32485fbe443f
nopalss <holanakdo@gmail.com>
2025-03-04 14:13:40 +0700

    fixed responsive

--- src/Pages/Home/Benefit.jsx
+++ src/Pages/Home/Benefit.jsx
@@ -28,32 +28,32 @@ const Benefit = () => {
         </div>
         <div className="w-full h-[200px] absolute top-0 bg-gradient-to-r from-[#46F883] to-[#FFDD00] opacity-60 blur-[80px] "></div>
       </div>
-      <div className="w-full grid grid-cols-1 xl:grid-cols-3 xl:gap-0 gap-10 sm:px-10 lg:px-30 py-10 justify-center left">
-        <div className="flex flex-row xl:flex-col gap-16  w-full  justify-center items-center ">
+      <div className="w-full grid grid-cols-1 lg:grid-cols-3  gap-10 sm:px-10 lg:px-30 py-10 justify-center left">
+        <div className="flex flex-row lg:flex-col gap-16  w-full  justify-center items-center ">
           <div className="flex flex-col gap-5 w-1/2  xl:w-[270px] ">
             <img src={star2} alt="" className="w-[48px] h-[48px]" />
-            <h1 className="text-2xl">Increased Efficiency</h1>
+            <h1 className="text-xl sm:text-2xl">Increased Efficiency</h1>
             <p className="text-sm">
               Our workflows are streamlined, optimised and built to perform.
             </p>
           </div>
           <div className=" flex flex-col gap-5 w-1/2  xl:w-[270px]">
             <img src={block} alt="" className="w-[48px] h-[48px] z-20" />
-            <h1 className="text-2xl">Great Communication</h1>
+            <h1 className="text-xl sm:text-2xl">Great Communication</h1>
             <p className="text-sm">
               We're here for you at any time of the day and would love to hear
               from you.
             </p>
           </div>
         </div>
-        <div className="xl:w-[430px] w-full flex justify-center  xl:justify-start relative">
+        <div className=" w-full flex justify-center  xl:justify-start relative">
           <img src={dashboard} alt="" className="w-[430px] z-20" />
           <div className="absolute w-full h-full rounded-full bg-gradient-to-r from-[#46F883] to-[#FFDD00] opacity-40 blur-[100px] top-0"></div>
         </div>
-        <div className="flex flex-row xl:flex-col gap-16  justify-center items-center">
+        <div className="flex flex-row lg:flex-col gap-16  xl:ml-0  justify-center items-center">
           <div className="  flex flex-col gap-5  w-1/2">
             <img src={block2} alt="" className="w-[48px] h-[48px]" />
-            <h1 className="text-2xl">Simple Systems</h1>
+            <h1 className="text-xl sm:text-2xl">Simple Systems</h1>
             <p className="text-sm">
               Sublime is easy to use and even easy to integrate with existing
               systems.
@@ -61,7 +61,7 @@ const Benefit = () => {
           </div>
           <div className=" flex flex-col gap-5 w-1/2">
             <img src={circles} alt="" className="w-[48px] h-[48px]" />
-            <h1 className="text-2xl">Easily Customized</h1>
+            <h1 className="text-xl sm:text-2xl">Easily Customized</h1>
             <p className="text-sm">
               Need a more custom solution? Done - let's chat about it.
             </p>
